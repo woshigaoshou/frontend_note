e0b8d3f9a29df2c6624ce3fad4f4138709a791a3
zhengguizhou <zhengguizhou@gridsum.com>
2022-01-27 16:25:57 +0800

    补充树结构非递归和前驱移除

--- 数据结构/BinarySearchTree.js
+++ 数据结构/BinarySearchTree.js
@@ -68,6 +68,7 @@ function BinarySearchTree() {
     }
     return false;
   },
+  // 非递归
   BinarySearchTree.prototype.preOrderTraverse = function(handler) {
     let stack = new Stack();
     let p = this.root;
@@ -84,6 +85,7 @@ function BinarySearchTree() {
   BinarySearchTree.prototype.inOrderTraverse = function(handler) {
     let stack = new Stack();
     let p = this.root;
+    // console.log(this.root);
     while(!stack.isEmpty() || p) {
       while(p) {
         stack.push(p);
@@ -94,55 +96,58 @@ function BinarySearchTree() {
       p = p.right;
     }
   },
-  // BinarySearchTree.prototype.preOrderTraverse = function(handler) {
-  //   let stack = new Stack();
-  //   let p = this.root;
-  //   while(!stack.isEmpty() || p) {
-  //     while(p) {
-  //       handler(p.key);
-  //       stack.push(p);
-  //       p = p.left;
-  //     }
-  //     p = stack.pop();
-  //     p = p.right;
-  //   }
-  // },
-  // BinarySearchTree.prototype.inOrderTraverse = function(handler) {
-  //   let stack = new Stack();
-  //   let p = this.root;
-  //   // console.log(this.root);
-  //   while(!stack.isEmpty() || p) {
-  //     while(p) {
-  //       stack.push(p);
-  //       p = p.left;
-  //     }
-  //     p = stack.pop();
-  //     handler(p.key);
-  //     p = p.right;
-  //   }
-  // },
-  // BinarySearchTree.prototype.postOrderTraverse = function(handler) {
-  //   let stack = new Stack();
-  //   let p = this.root;
-  //   let visited = null;
-  //   while(!stack.isEmpty() || p) {
-  //     while(p) {
-  //       stack.push(p)
-  //       p = p.left;
-  //     }
-  //     p = stack.pop();
-  //     // 颜色标记法，右节点已访问过则不再访问(右节点总是在该节点上一次处理)
-  //     if (p.right === null || p.right === visited) {
-  //       handler(p.key);
-  //       visited = p;
-  //       p = null;  // 置空，跳过处理左节点的步骤
-  //     } else {
-  //       // 处理右节点
-  //       stack.push(p);
-  //       p = p.right;
-  //     }
-  //   }
-  // },
+  BinarySearchTree.prototype.postOrderTraverse = function(handler) {
+    let stack = new Stack();
+    let p = this.root;
+    let visited = null;
+    while(!stack.isEmpty() || p) {
+      while(p) {
+        stack.push(p)
+        p = p.left;
+      }
+      p = stack.pop();
+      // 颜色标记法，右节点已访问过则不再访问(右节点总是在该节点上一次处理)
+      if (p.right === null || p.right === visited) {
+        handler(p.key);
+        visited = p;
+        p = null;  // 置空，跳过处理左节点的步骤
+      } else {
+        // 处理右节点
+        stack.push(p);
+        p = p.right;
+      }
+    }
+  },
+  /* 递归实现
+  // 先序遍历，先处理自身，再处理左节点，最后处理右节点
+  BinarySearchTree.prototype.preOrderTraverse = function(handler) {
+    this.preOrderTraverseNode(this.root, handler);
+  },
+  BinarySearchTree.prototype.preOrderTraverseNode = function(node, handler) {
+    handler(node.key);
+    node.left && this.preOrderTraverseNode(node.left, handler);
+    node.right && this.preOrderTraverseNode(node.right, handler)
+  },
+  // 中序遍历，先处理左节点，再处理自身，最后处理右节点
+  BinarySearchTree.prototype.inOrderTraverse = function(handler) {
+    this.inOrderTraverseNode(this.root, handler);
+  },
+  BinarySearchTree.prototype.inOrderTraverseNode = function(node, handler) {
+    node.left && this.inOrderTraverseNode(node.left, handler);
+    handler(node.key);
+    node.right && this.inOrderTraverseNode(node.right, handler)
+  },
+  // 后序遍历，先处理左节点，再处理右节点，最后处理自身
+  BinarySearchTree.prototype.postOrderTraverse = function(handler) {
+    this.postOrderTraverseNode(this.root, handler);
+  },
+  BinarySearchTree.prototype.postOrderTraverseNode = function(node, handler) {
+    handler(node.key);
+    node.left && this.postOrderTraverseNode(node.left, handler);
+    node.right && this.postOrderTraverseNode(node.right, handler)
+  },
+  */
+  // 后继节点
   BinarySearchTree.prototype.remove = function(key) {
     let current = this.root;
     let parentNode = null;
@@ -193,6 +198,58 @@ function BinarySearchTree() {
     }
     return successor;
   }
+  /*
+  前驱节点
+  BinarySearchTree.prototype.remove = function(key) {
+    let current = this.root;
+    let isLeftNode = true;  // 记录移除节点是否是父节点的左节点
+    let parentNode = null;  // 记录移除节点的父节点
+    while (current && current.key !== key) {
+      parentNode = current;
+      if (current.key < key) {
+        isLeftNode = false;
+        current = current.right;
+      } else if (current.key > key) {
+        isLeftNode = true;
+        current = current.left;
+      }
+    }
+    if (current === null) return false;
+    else if (current.left === null && current.right === null) {
+      if (current === this.root) this.root = null;
+      parentNode[isLeftNode ? 'left' : 'right'] = null;
+    } else if (current.right === null) {
+      if (current === this.root) this.root = this.root.left;
+      parentNode[isLeftNode ? 'left' : 'right'] = current.left;
+    } else if (current.left === null) {
+      if (current === this.root) this.root = this.root.right;
+      parentNode[isLeftNode ? 'left' : 'right'] = current.right;
+    } else {
+      // 寻找删除节点的前驱后继节点(左边的最右节点，右边的最左节点)
+      const successor = this.getSuccessor(current);
+      if (this.root === current) {
+        this.root = successor;
+      } else {
+        parentNode[isLeftNode ? 'left' : 'right'] = successor;
+      }
+      successor.right = current.right;
+    }
+  },
+  BinarySearchTree.prototype.getSuccessor = function(delNode) {
+    let successor = delNode.left;
+    let successorParent = delNode;
+    while(successor.right) {
+      successorParent = successor;
+      successor = successor.right;
+    }
+    // 判断删除节点是否是前驱节点的父节点
+    if (delNode !== successorParent) {
+      successorParent.right = successor.left;
+      successor.left = delNode.left;
+    }
+    return successor;
+  }
+  */
 }
 
   // const bst = new BinarySearchTree();
